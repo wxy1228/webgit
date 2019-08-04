13354f7d316662a374b0f8dc58a50003fb53981c
wxy1228 <53605053+wxy1228@users.noreply.github.com>
2019-08-04 18:14:40 +0800

    Add files via upload

---
+++ precache-manifest.bbe2972b14a2738cc017cdb2b2db922a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cfeebb2463c34dfe82d5e4176b70ce6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c78313e1215baf8e0bd",
+    "url": "/static/css/2.1be8104b.chunk.css"
+  },
+  {
+    "revision": "7df27fc63c5d5cfe4ddc",
+    "url": "/static/css/main.1a7d512e.chunk.css"
+  },
+  {
+    "revision": "7c78313e1215baf8e0bd",
+    "url": "/static/js/2.88989e8f.chunk.js"
+  },
+  {
+    "revision": "7df27fc63c5d5cfe4ddc",
+    "url": "/static/js/main.dca148fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
